b9badb4cfdd83b310caad426d2016f2e9dc38773
SteveFinch <steve@localhost.localdomain>
2018-10-02 11:56:44 -0400

    Lower power builder

--- Builder.js
+++ Builder.js
@@ -10,40 +10,49 @@
 module.exports = {
     type: 'builder',
     min: function() {return 0;},
-    max: function() {reqE = _.sum( Game.constructionSites, (x) => x.progressTotal - x.progress ); nB = Math.ceil( reqE/50000 ); if( nB > 4 ) {return 4;} else {return (nB == 0 ? (reqE > 0 ? 1 : 0) : nB) }},
+    max: function() {
+      let reqE = _.sum( Game.constructionSites, (x) => (x.progressTotal - x.progress) );
+      let nB = Math.ceil( reqE/30000 );
+      if( nB > 4 ) {
+        return 4;
+      } else {
+        return (nB == 0 ? (reqE > 0 ? 1 : 0) : nB)
+      }
+    },
     max_energy: function() {return 1200;},
-    work_sequence: function() {return ['upgrader'];},
 
    run: function( creep ) {
        repairer = require('Repairer');
-       upgrader = require('TopUp');
-       brickie = require('Brickie');
        common = require('Common');
-       
+
        if( common.gotoFlag( creep ) < 0 ) {
             return;
-        }  
+        }
+
         if( common.checkWorking(creep ) == true ) {
-            
-            var cS = _.map(Game.constructionSites, (x,y) => x);
-            //console.log( "LCS="+ cS );
-            var constructionSite = creep.pos.findClosestByPath( cS ); //, {
-            //    filter: (x) => creep.memory.room != undefined ? x.pos.roomName == creep.memory.room : true
-            //});
-            // if one is found
-            
-            if( constructionSite == undefined && cS.length > 0 ) {
-                constructionSite = cS[0];
+            let constructionSite;
+            if( creep.memory.current_site == undefined ||
+                ((Game.time % 7) == 6) ||
+                (constructionSite = Game.getObjectById( creep.memory.current_site)) == undefined ) {
+                  let cS = _.map(Game.constructionSites, (x,y) => x);
+                  let constructionSite = creep.pos.findClosestByPath( cS );
+
+                  if( constructionSite == undefined && cS.length > 0 ) {
+                      constructionSite = cS[0];
+                  }
+                  if( constructionSite != undefined ) {
+                    creep.memory.current_site = constructionSite.id;
+                  } else {
+                    creep.memory.current_site = undefined;
+                  }
             }
-            
             if (constructionSite != undefined) {
                 console.log( creep.name + " going to construct " + constructionSite)
                 // try to build, if the constructionSite is not in range
                 if ((rv = creep.build(constructionSite)) == ERR_NOT_IN_RANGE) {
-                    // move towards the constructionSite
                     creep.moveTo(constructionSite);
                 } else {
-                    console.log(creep.name + " received rval = " + rv);
+                    console.log(creep.name + " received rval = " + rv + " while trying to build " + constructionSite );
                 }
                 return OK;
             } else {
@@ -51,10 +60,8 @@ module.exports = {
                 return repairer.run( creep ); /// should be decision of master controller
             }
         } else {
-            
-                common.fillHerUp( creep );
-                return ERR_NOT_ENOUGH_ENERGY;
-
+            common.fillHerUp( creep );
+            return ERR_NOT_ENOUGH_ENERGY;
         }
     }
 };
