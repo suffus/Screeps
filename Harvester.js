68613b40970b3f15fb3d6c3f81e83335b3c53566
SteveFinch <steve@localhost.localdomain>
2018-10-03 16:43:26 -0400

    db

--- Harvester.js
+++ Harvester.js
@@ -120,7 +120,7 @@ module.exports = {
             }
             err = common.fillHerUp( creep, undefined, undefined, function(x) {
                     return common.isInRegion( x.pos, 'spawnRegion' ) &&
-                            x.pos.roomName == creep.memory.room );
+                            x.pos.roomName == creep.memory.room;
               });
             if( err = ERR_NOT_ENOUGH_RESOURCES ) {
               console.log( "NO SOURCES FOR HARVESTER " + creep.name );
