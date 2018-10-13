a9afa7d665acb9ad74189831df5adbd289c2bb84
Steve Finch <karl@new-host.home>
2018-10-13 11:33:04 -0400

    db

--- TraderJoe.js
+++ TraderJoe.js
@@ -4,6 +4,25 @@
 module.exports = {
     type: 'trader',
 
+
+    doTrade: function( ) {
+              if( Memory.trades == undefined ) {
+                return ERR_NOT_FOUND;
+              }
+              let orders = Game.market.getAllOrders();
+              for( trade of Memory.trades ) {
+                if( trade.tradeType == ORDER_SELL ) {
+                  let validOrders = _.filter( orders, (x) => x.resourceType == trade.resourceType && x.type == ORDER_BUY
+                                 && Game.market.calcTransactionCost( 1000, x.roomName, trade.roomName ) <= trade.maxCPM );
+
+                  console.log( "There are " + validOrders.length + " valid orders");
+                }
+              }
+              return OK;
+
+    },
+
+
     pushTrade: function( resType, room, maxAmount, type, maxCPM ) {
       let trade = {
         'resourceType': resType,
